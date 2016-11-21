1f2487bf7ad1453e405ad29ca0dd650b236690ce
Joe S <joe@pixelbash.co.nz>
2016-11-20 23:54:15 -0500

    Remove commented out express function

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -1,48 +1,5 @@
 module Spree
   class PaypalController < StoreController
-
-    # def express
-    #   order = current_order || raise(ActiveRecord::RecordNotFound)
-    #   items = order.line_items.map(&method(:line_item))
-
-    #   additional_adjustments = order.all_adjustments.additional
-    #   tax_adjustments = additional_adjustments.tax
-    #   shipping_adjustments = additional_adjustments.shipping
-
-    #   additional_adjustments.eligible.each do |adjustment|
-    #     next if (tax_adjustments + shipping_adjustments).include?(adjustment)
-    #     items << {
-    #       :Name => adjustment.label,
-    #       :Quantity => 1,
-    #       :Amount => {
-    #         :currencyID => order.currency,
-    #         :value => adjustment.amount
-    #       }
-    #     }
-    #   end
-
-    #   # Because PayPal doesn't accept $0 items at all.
-    #   # See #10
-    #   # https://cms.paypal.com/uk/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_api_ECCustomizing
-    #   # "It can be a positive or negative value but not zero."
-    #   items.reject! do |item|
-    #     item[:Amount][:value].zero?
-    #   end
-    #   pp_request = provider.build_set_express_checkout(express_checkout_request_details(order, items))
-
-    #   begin
-    #     pp_response = provider.set_express_checkout(pp_request)
-    #     if pp_response.success?
-    #       redirect_to provider.express_checkout_url(pp_response, :useraction => 'commit')
-    #     else
-    #       flash[:error] = Spree.t('flash.generic_error', :scope => 'paypal', :reasons => pp_response.errors.map(&:long_message).join(" "))
-    #       redirect_to checkout_state_path(:payment)
-    #     end
-    #   rescue SocketError
-    #     flash[:error] = Spree.t('flash.connection_failed', :scope => 'paypal')
-    #     redirect_to checkout_state_path(:payment)
-    #   end
-    # end
     def express
      order = current_order || raise(ActiveRecord::RecordNotFound)
      items = order.line_items.map(&method(:line_item))
